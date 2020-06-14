8222e47cef8db6433fdb375a3513ef843409bac3
anhtuan240599 <55918598+anhtuan240599@users.noreply.github.com>
2020-06-14 13:07:36 +0700

    16/4/2020

---
+++ WatchShop/WatchShop/Controllers/BrandController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatchShop.Models;
+
+namespace WatchShop.Controllers
+{
+    public class BrandController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        // GET: Brand
+        public ActionResult Index()
+        {
+            List<Brand> listBrand = dbContext.Brands.ToList();//database
+            return View(listBrand);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Brand brand)
+        {
+            dbContext.Brands.Add(brand);
+            dbContext.SaveChanges();
+            //luu vao database
+            return RedirectToAction("Brands", "Admin");
+        }
+    }
+}
---
+++ WatchShop/WatchShop/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatchShop.Models;
+
+namespace WatchShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            List<Item> listItem = dbContext.Items.ToList();//database
+            return View(listItem);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
